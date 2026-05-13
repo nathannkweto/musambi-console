a2a9d034bee4d61ab799078bcdbdda4918c0a5f4
Nathan Nkweto <nathannkweto@nkweto.tech>
2026-05-13 18:46:30 +0100

    ssr: add ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,15 +3,15 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {authInterceptor} from './core/interceptors/auth.interceptor';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(
-      withInterceptors([authInterceptor])
-    )
-  ]
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideClientHydration(withEventReplay()),
+  ],
 };
